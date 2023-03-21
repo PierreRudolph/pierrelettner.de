66bba2857c93a1f829ad3ac9b3974525d3d9c0a1
PierreRudolph <Plettner@hotmail.de>
2023-03-21 18:51:49 +0100

    added data-protection and imprint and menu_darkmode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pierre Lettner';
+
+
+  constructor(public router: Router) { }
+
+
+
 }
